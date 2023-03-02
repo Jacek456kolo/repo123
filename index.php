3f64850ce394f3e7f93792863f221f9e1ebda1ae
Jacek Gach <projekt@ceti.com.pl>
2023-03-02 15:49:00 +0100

    coto

--- index.php
+++ index.php
@@ -14,8 +14,7 @@
 	  background-color: yellow;
 	}
   
-  h1 {text-align:center}
-	  
+  	  
 	  #naglowek {
 		  font-size:24px;
 		  text-align:center;
@@ -26,17 +25,75 @@
 		  text-align:center;
 	  }
 	  
+	  #naglowek2 {
+		  font-size:10px;
+		  text-align:center;
+	  }
+	  
+	  #naglowek3 {
+		  font-size:12px;
+		  text-align:center;
+	  }
+	  
+	  #naglowek4 {
+		  font-size:12px;
+		  text-align:center;
+  		  margin-bottom: 24px;
+	  }
+	  
+	  #naglowek5 {
+		  font-size:12px;
+		  text-align:center;
+	  }
+	  
+	  
+	  #naglowek6 {
+		  font-size:12px;
+		  text-align:center;
+		  }
+	  
+	  #naglowek7 {
+		  font-size:12px;
+		  text-align:center;
+		  margin-bottom: 24px;
+	  }
+	  
+	  #logo {
+		  margin-bottom: 24px;
+		  text-align: center;
+	  }
+	  
+	  #kolumny
+	  {
+		display:flex;  
+		margin-left: 10px;
+		margin-right: 10px;
+	  }
+	  
+	  
     </style>
 </head>
 <body>
-		<p style="text-align: right;"/p>
-		<h1><?php echo 'Hello, World!'; ?></h1>
-		<p>jklkllk</p>
-		<p style="text-align: right;">sroda</p>
-		<p style="text-align:center;"/p>
-		<img src="logo2.jpg">
+	
+		
+		<div id="logo"> <img src="logo2.jpg"></div>
 		<div id="naglowek">CERTYFIKAT</div>
 		<div id="naglowek1">AUTENTYCZNOSCI</div>
+<!-- -->		<p style "margin-bottom: 24px"></p>
+		<div id="naglowek3">potwierdza legalnosc oprogramowania</div>
+		<div id="naglowek4">firmy GDATA Software</div>
+		
+		<div id="naglowek6">Licencja i klucz licencyjny</div>
+		<div id="naglowek7">Antivirus OEM 1PC 1ROK</div>
+		
+		<div id="kolumny">
+		<div>There is more than one way to create HTML columns
+		</div>
+		<div>Obrazy dla how to create column in css
+		</div>
+		<div>The columns CSS shorthand property sets the number 
+		</div>
+		</div>
 		
 		
 </body>
---
+++ index2.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Hello, World!</title>
+	<style> 
+	
+	p {
+    
+	}
+	
+	html {
+	  background-color: yellow;
+	}
+  
+  h1 {text-align:center}
+	  
+	  #naglowek {
+		  font-size:24px;
+		  text-align:center;
+	  }
+	  
+	  #naglowek1 {
+		  font-size:18px;
+		  text-align:center;
+	  }
+	  
+    </style>
+</head>
+<body>
+		<p style="text-align: right;"/p>
+		<h1><?php echo 'Hello, World!'; ?></h1>
+		<p>jklkllk</p>
+		<p style="text-align: right;">sroda</p>
+		<p style="text-align:center;"/p>
+		<div> <img src="logo2.jpg"></div>
+		<div id="naglowek">CERTYFIKAT</div>
+		<div id="naglowek1">AUTENTYCZNOSCI</div>
+		
+		
+</body>
+</html> 
