171493ad277ea433a2b26f4799beed72fd82f0f9
Jacek Gach <projekt@ceti.com.pl>
2023-03-02 13:44:23 +0100

    cos

--- index.php
+++ index.php
@@ -4,12 +4,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP - Hello, World!</title>
+	<style> 
+	
+	p {
+    
+	}
+	
+	html {
+	  background-color: yellow;
+	}
+  
+  h1 {text-align:center}
+	  
+	  #naglowek {
+		  font-size:24px;
+		  text-align:center;
+	  }
+	  
+	  #naglowek1 {
+		  font-size:18px;
+		  text-align:center;
+	  }
+	  
+    </style>
 </head>
 <body>
-        <h1><?php echo 'Hello, World!'; ?></h1>
+		<p style="text-align: right;"/p>
 		<h1><?php echo 'Hello, World!'; ?></h1>
-		<h1><?php echo 'Hello, World!'; ?></h1>
-		<h1><?php echo 'Hello, World!'; ?></h1>
-		<h1><?php echo 'Hello, World!'; ?></h1>
+		<p>jklkllk</p>
+		<p style="text-align: right;">sroda</p>
+		<p style="text-align:center;"/p>
+		<img src="logo2.jpg">
+		<div id="naglowek">CERTYFIKAT</div>
+		<div id="naglowek1">AUTENTYCZNOSCI</div>
+		
+		
 </body>
-</html>
+</html> 
---
+++ kopia.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Hello, World!</title>
+	<style> p {
+    }
+	html {
+	background-color: yellow;
+	}
+  </style>
+</head>
+<body>
+		<h1><?php echo 'Hello, World!'; ?></h1>
+		<p>jklkllk</p>
+</body>
+</html> 
